4e00baf5363fa32effbd57da0eed45f3092e3b03
houhan@gmail.com <han.hou@alleninstitute.org>
2024-09-22 03:21:06 +0000

    feat: upload results to docDB and s3

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,64 @@
 """ top level run script """
 
+import glob
+from tqdm import tqdm
+import os
+import json
+
+from utils.docDB_io import (
+    insert_result_to_docDB_ssh,
+    update_job_manager,
+    DocumentDbSSHClient,
+    credentials,
+)
+from utils.aws_io import upload_result_to_s3
+
+S3_RESULTS_ROOT = f's3://aind-behavior-data/foraging_nwb_bonsai_processed/v2/'
+
+
 def run():
-    """ basic run function """
-    pass
+    # In /root/capsule/data/aind_analysis_arch_pipeline_results, use glob to find all docDB_job_manager.json files
+    all_jobs_jsons = glob.glob('/root/capsule/data/aind_analysis_arch_pipeline_results/**/docDB_job_manager.json', recursive=True)
+    
+    with DocumentDbSSHClient(credentials=credentials) as doc_db_client:
+        for job_json in tqdm(all_jobs_jsons, desc='Processing jobs', total=len(all_jobs_jsons)):
+            result_folder = os.path.dirname(job_json)
+            job_hash = os.path.basename(os.path.dirname(job_json))
+           
+            with open(job_json, 'r') as f:
+                job_dict = json.load(f)
+
+            collection_name = job_dict['collection_name']
+                
+            if job_dict['status'] == "success":
+                result_json = os.path.join(result_folder, f"docDB_{collection_name}.json")
+                with open(result_json, 'r') as f:
+                    result_dict = json.load(f)
+                                
+                # -- Insert result_dict to docDB --
+                insert_result_response = insert_result_to_docDB_ssh(
+                    result_dict=result_dict,
+                    collection_name=collection_name,
+                    doc_db_client=doc_db_client,
+                )
+                job_dict.update(insert_result_response)
+                
+                # -- Upload results to s3 --
+                s3_path = S3_RESULTS_ROOT + job_hash
+                upload_result_to_s3(
+                    result_folder + '/', 
+                    s3_path + '/',
+                )
+                job_dict.update({
+                    "s3_location": s3_path,
+                })
+            
+            # -- Update job_manager --
+            response = update_job_manager(
+                job_hash=job_hash,
+                update_dict=job_dict,
+                doc_db_client=doc_db_client,
+            )
+    
 
 if __name__ == "__main__": run()
---
+++ code/utils/docDB_io.py
@@ -0,0 +1,104 @@
+import logging
+import time
+from functools import wraps
+from pymongo.errors import ServerSelectionTimeoutError
+from sshtunnel import HandlerSSHTunnelForwarderError
+
+SSH_ERRORS = [ServerSelectionTimeoutError, HandlerSSHTunnelForwarderError]
+
+import traceback
+from datetime import datetime
+
+from aind_data_access_api.document_db_ssh import DocumentDbSSHClient, DocumentDbSSHCredentials
+
+credentials = DocumentDbSSHCredentials()
+credentials.database = "behavior_analysis"
+
+logger = logging.getLogger(__name__)
+
+MAX_SSH_RETRIES = 50
+TIMEOUT = 5 # Time in seconds between retries
+
+def retry_on_ssh_timeout(max_retries=MAX_SSH_RETRIES, timeout=TIMEOUT):
+    """
+    Decorator to retry a function upon ServerSelectionTimeoutError.
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            retries = 0
+            while retries < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                # Suppose all other exceptions have been captured inside func!
+                except SSH_ERRORS as e:  
+                    retries += 1
+                    msg = f"{str(e)}\n{traceback.format_exc()}\nRetry {retries}/{max_retries}..."
+                    logger.warning(msg)
+                    print(msg, flush=True)
+                    if retries >= max_retries:
+                        logger.error("Max retries reached. Raising exception.")
+                        raise
+                    time.sleep(timeout)
+        return wrapper
+    return decorator
+
+def insert_result_to_docDB_ssh(result_dict, collection_name, doc_db_client) -> dict:
+    """_summary_
+
+    Parameters
+    ----------
+    result_dict : dict
+        bson-compatible result dictionary to be inserted
+    collection_name : _type_
+        name of the collection to insert into (such as "mle_fitting")
+
+    Returns
+    -------
+    dict
+        docDB upload status
+    """
+    doc_db_client.collection_name = collection_name
+    db = doc_db_client.collection
+
+    # Check if job hash already exists, if yes, log warning, but still insert
+    if db.find_one({"job_hash": result_dict["job_hash"]}):
+        logger.warning(f"Job hash {result_dict['job_hash']} already exists in {collection_name} in docDB")
+    # Insert (this will add _id automatically to result_dict)
+    response = db.insert_one(result_dict)
+    result_dict["_id"] = str(result_dict["_id"])
+
+    if response.acknowledged is False:
+        logger.error(f"Failed to insert {result_dict['job_hash']} to {collection_name} in docDB")
+        return {"docDB_upload_status": "docDB insertion not acknowledged", "docDB_id": None, "collection_name": None}
+    else:
+        logger.info(f"Inserted {response.inserted_id} to {collection_name} in docDB")
+        return {"docDB_upload_status": "success", "docDB_id": response.inserted_id, "collection_name": collection_name}
+
+
+def update_job_manager(job_hash, update_dict, doc_db_client):
+    """_summary_
+
+    Parameters
+    ----------
+    job_hash : _type_
+        _description_
+    status : _type_
+        _description_
+    log : _type_
+        _description_
+    """
+    doc_db_client.collection_name = "job_manager"
+    db = doc_db_client.collection
+    
+    # Check if job hash already exists, if yes, log warning, but still insert
+    if not db.find_one({"job_hash": job_hash}):
+        logger.warning(f"Job hash {job_hash} does not exist in job_manager in docDB! Skipping update.")
+        return
+    
+    # Update job status and log
+    response = db.update_one(
+        {"job_hash": job_hash},
+        {"$set": update_dict},
+    )
+    
